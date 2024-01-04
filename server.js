abc03c82c2983647070dcb5e324828f00f8f2319
Смирнов Алексей <79068122307@yandex.ru>
2024-01-04 14:25:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/todo"));
+app.use(express.static("./dist/todo/browser"));
 
 app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/todo/browser/" }));
 
