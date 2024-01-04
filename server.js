a0a1ce1b67c7ba63b7205357390d552e614d50c3
Смирнов Алексей <79068122307@yandex.ru>
2024-01-04 14:16:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static("./dist/todo"));
 
-app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/todo/" }));
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/todo/browser/" }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
